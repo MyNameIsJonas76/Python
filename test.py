7ca48562245195f9e6cdb9b8e88b88d623a5aeed
MyNameIsJonas76 <36041517+MyNameIsJonas76@users.noreply.github.com>
2018-02-01 15:28:48 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+import xlwings as xw
+import pandas as pd
+
+df = pd.read_excel(r'C:\Users\n429310\Documents\squash.xlsx', sheetname="Blad1")
+
+participants = len(df)+1
+print ("Number of participants: %s" %participants)
